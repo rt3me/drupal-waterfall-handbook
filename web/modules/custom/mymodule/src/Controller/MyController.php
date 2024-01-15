1f02bf35b686e347aeb0851d527ad6f9f4e8db20
Robert Turner <robturneris@gmail.com>
2024-01-15 15:14:17 -0500

    add variable content method and route

--- web/modules/custom/mymodule/src/Controller/MyController.php
+++ web/modules/custom/mymodule/src/Controller/MyController.php
@@ -20,4 +20,15 @@ class MyController extends ControllerBase
       '#markup' => t('Hello Drupal world. Time flies like an arrow, fruit flies like a banana.'),
     ];
   }
+
+  public function variableContent($name_1, $name_2)
+  {
+    return [
+      '#type' => 'markup',
+      '#markup' => t(
+        '@name1 and @name2 say hello to you!',
+        ['@name1' => $name_1, '@name2' => $name_2]
+      ),
+    ];
+  }
 }
