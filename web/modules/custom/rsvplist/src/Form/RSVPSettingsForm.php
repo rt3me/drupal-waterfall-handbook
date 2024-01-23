f9ffa8ef2b15c2acbdfac01eb54328247a0d10f7
Robert Turner <robturneris@gmail.com>
2024-01-23 02:41:19 -0500

    correct code standard violations

--- web/modules/custom/rsvplist/src/Form/RSVPSettingsForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPSettingsForm.php
@@ -1,15 +1,18 @@
 <?php
 
-/** 
- * @file
- * Contains the settings for administering the RSVP Form
- */
-
 namespace Drupal\rsvplist\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * @file
+ * Contains the settings for administering the RSVP Form.
+ */
+
+/**
+ * {@inheritdoc}
+ */
 class RSVPSettingsForm extends ConfigFormBase {
 
   /**
@@ -19,7 +22,7 @@ class RSVPSettingsForm extends ConfigFormBase {
     return 'rsvplist_admin_settings';
   }
 
-  /** 
+  /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
@@ -32,12 +35,15 @@ class RSVPSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // return an array of strings of the labels of node content types currently existing on the site
+    // Return an array of strings of the labels of node content
+    // types currently existing on the site.
     $types = node_type_get_names();
-    // a method of the ConfigFormBase class that retrieves the configuration object
+    // A method of the ConfigFormBase class that retrieves the
+    // configuration object.
     $config = $this->config('rsvplist.settings');
-    // allowed_types requires its own file
-    // do not need to declare a submit button in the form render array like RSVPForm
+    // Allowed_types requires its own file.
+    // Do not need to declare a submit button in the form render
+    // array like RSVPForm.
     $form['rsvplist_types'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('The content types to enable RSVP collection for'),
@@ -47,7 +53,7 @@ class RSVPSettingsForm extends ConfigFormBase {
         will be available and can be enabled while the node is being edited.'),
     ];
 
-    // no need for a submit button in the form render array because of this
+    // No need for a submit button in the form render array because of this.
     return parent::buildForm($form, $form_state);
   }
 
@@ -55,7 +61,7 @@ class RSVPSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // remove empty strings
+    // Remove empty strings.
     $selected_allowed_types = array_filter($form_state->getValue(
       'rsvplist_types'));
     sort($selected_allowed_types);
@@ -63,7 +69,8 @@ class RSVPSettingsForm extends ConfigFormBase {
     $this->config('rsvplist.settings')
       ->set('allowed_types', $selected_allowed_types)
       ->save();
-    
+
     parent::submitForm($form, $form_state);
   }
+
 }
